16640103a6abbde97e51f710acff9b1ed0326986
Nathaniel Wright <nathan@nathanieljwright.com>
2024-02-03 09:18:41 -0800

    New start

--- test/socket_test.c
+++
@@ -1,6 +0,0 @@
-﻿#include "sprig_socket.h"
-#include <stdlib.h>
-int main(int argc, char *argv[]) {
-    sprig_socket sock = sprig_socket_bind("www.google.com", "443");
-    sprig_socket local_sock = sprig_socket_bind(NULL, "10");
-}
