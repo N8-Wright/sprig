5d92c5e30795129fb40c3452322741faf62fd899
nicfv <nfmventura@gmail.com>
2024-03-07 20:37:44 -0800

    Update config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,21 +1,20 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './src/app.ts',
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: /node_modules/,
             },
         ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['.ts'],
     },
     output: {
-        filename: 'bundle.js',
+        filename: 'app.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };
