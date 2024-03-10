328aca89a8d5b80b559742626ca605d5fb480763
nicfv <nfmventura@gmail.com>
2024-03-09 20:05:53 -0800

    More webpack simplifications

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,8 @@
-const path = require('path');
-
 module.exports = {
-    mode: 'production',
     entry: './src/app.ts',
     module: {
         rules: [
             { test: /\.tsx?$/, use: 'ts-loader' },
         ],
-    },
-    resolve: {
-        extensions: ['.ts'],
-    },
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'dist'),
     },
 };
