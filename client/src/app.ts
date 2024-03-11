b6daffa0f0bfcb89f983f2c40a9cb334a4cf83a2
nicfv <envneo+github@gmail.com>
2024-03-11 15:53:32 -0700

    Wow! I never knew about script defer! This changes everything! (not by much, but it is still great)

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,8 +1,4 @@
 /**
  * This is the entry point to the client application.
  */
-function main(): void {
-    console.log('Loaded!');
-}
-
-window.addEventListener('load', main);
+console.log('Loaded!');
